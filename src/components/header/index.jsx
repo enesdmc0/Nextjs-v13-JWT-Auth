4c129ab48aa2801d387039609e9ff92274bbe375
enesdmc0 <20194604004@ogr.akdeniz.edu.tr>
2023-04-11 21:08:20 +0300

    header update

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,14 +1,17 @@
+import { useAuth } from "@/hooks/useAuth";
 import Link from "next/link";
 
-const Header = () => {
+const Header = async () => {
+    const auth = await useAuth.fromServer()
     return (
         <header className="bg-gray-400 fixed top-0 w-full flex p-7 items-center justify-between">
             <div className="bg-gray-300 p-2 rounded">
                 <Link href="/">EnesDmc</Link>
             </div>
             <nav className="bg-gray-300 p-2 rounded flex gap-5">
-                <Link href="/login">Login</Link>
-                <Link href="/profile">Profile</Link>
+                {auth  
+                ? <Link href="/login">Login</Link>
+                : <Link href="/profile">Profile</Link>}
             </nav>
         </header>
     );
